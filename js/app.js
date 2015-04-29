810d7e3223272cabe5cda96d0b6daebc03695860
João Trepichio <trepichio@gmail.com>
2015-04-29 11:34:56 -0300

    Added multiple cats! Added Cat list on html and can select one by clicking on its name

--- js/app.js
+++ js/app.js
@@ -1,3 +1,49 @@
+var initialCats = [
+	{
+		name : 'Tobby',
+		imgSrc :'img/1413379559_412a540d29_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['TabTab', 'T-Bone', 'Mr. T']
+	},
+	{
+		name : 'Stuart',
+		imgSrc : 'img/22252709_010df3379e_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Stu']
+	},
+	{
+		name : 'Galileo',
+		imgSrc :'img/4154543904_6e2428c421_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Gaa']
+	},
+	{
+		name : 'José',
+		imgSrc :'img/434164568_fea0ad4013_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Zé']
+	},
+	{
+		name : 'Sofia',
+		imgSrc :'img/9648464288_2516b35537_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Soffi']
+	},
+	{
+		name : 'Pink',
+		imgSrc :'img/cat_1.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Tigger']
+	},
+
+];
+
 var cat = function (data) {
 	this.name = ko.observable(data.name);
 	this.imgSrc = ko.observable(data.imgSrc);
@@ -20,15 +66,21 @@ var cat = function (data) {
 
 var viewModel = function() {
 	var self = this;
-	self.currentCat = ko.observable(new cat({
-		name : 'Tobby',
-		imgSrc :'img/1413379559_412a540d29_z.jpg',
-		clickCount : 0,
-		levelCount : 0,
-		nicknames : ['Kitty', 'Tob', 'Kat']
-	}));
+
+	self.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(pet){
+		self.catList.push( new cat(pet) );	
+	});
+
+	self.currentCat = ko.observable(self.catList()[0]);
+	
 	self.incrementCounter = function(){
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	self.selectCat = function(cat){
+		self.currentCat(cat);
 	};
 
 
