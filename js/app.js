3da75f3f608e6eb61a009844b4efc8915261f764
João Trepichio <trepichio@gmail.com>
2015-04-29 08:17:57 -0300

    Moved the cat model from viewModel to its own Constructor and modified references on both js and html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var viewModel = function() {
+var cat = function () {
 	this.name = ko.observable('Tobby');
 	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
 	this.clickCount = ko.observable(0);
@@ -7,7 +7,6 @@ var viewModel = function() {
 		'newborn','infant','teen','adult','old','very old','IT SHOULD BE probably dead','shit! this cat is immortal!'
 	];
 	this.level = ko.computed(function(){
-		console.log(this.levelCount());
 		var result = this.levels[this.levelCount()];
 		if (this.clickCount() % 10 == 0 && this.levelCount() < this.levels.length - 1) {
 			this.levelCount(this.levelCount()+1);
@@ -16,13 +15,18 @@ var viewModel = function() {
 
 	},this);
 
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
-
 	this.nicknames = ko.observableArray([
 		'Kitty', 'Tob', 'Kat'
 	]);
+};
+
+var viewModel = function() {
+	this.currentCat = ko.observable(new cat());
+	this.incrementCounter = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+
+
 };
 
 ko.applyBindings(new viewModel());
