fb8c33f9817b2967fff8326ccba6c9f126c35cab
João Trepichio <trepichio@gmail.com>
2015-04-29 10:17:24 -0300

    Cat Constructor now receives an obj as param to fill its attributes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-var cat = function () {
-	this.name = ko.observable('Tobby');
-	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
-	this.clickCount = ko.observable(0);
-	this.levelCount = ko.observable(0);
+var cat = function (data) {
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.clickCount = ko.observable(data.clickCount);
+	this.levelCount = ko.observable(data.levelCount);
 	this.levels = [
 		'newborn','infant','teen','adult','old','very old','IT SHOULD BE probably dead','shit! this cat is immortal!'
 	];
@@ -15,14 +15,18 @@ var cat = function () {
 
 	},this);
 
-	this.nicknames = ko.observableArray([
-		'Kitty', 'Tob', 'Kat'
-	]);
+	this.nicknames = ko.observableArray(data.nicknames);
 };
 
 var viewModel = function() {
 	var self = this;
-	self.currentCat = ko.observable(new cat());
+	self.currentCat = ko.observable(new cat({
+		name : 'Tobby',
+		imgSrc :'img/1413379559_412a540d29_z.jpg',
+		clickCount : 0,
+		levelCount : 0,
+		nicknames : ['Kitty', 'Tob', 'Kat']
+	}));
 	self.incrementCounter = function(){
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
